75e8869fcb57b6339f59d1c6746b353fab9eabb2
megadoro <6.626068h@gmail.com>
2012-05-14 21:47:45 +0200

    update

--- ODE Viewer/ode.h
+++ ODE Viewer/ode.h
@@ -13,16 +13,32 @@ double vanderpol(double x,double v,double t){
     return 4*(1-x*x)*v-x;
 }
 
+double lorenz_x(double x,double y,double z){
+    return 10*(y-x);
+}
+
+double lorenz_y(double x,double y,double z){
+    return x*(28-z)-y;
+}
+
+double lorenz_z(double x,double y,double z){
+    return x*y-8*z/3;
+}
+
 typedef struct _plist{
-    double x,y,t;
+    double x,vx,y,vy,z,vz,t;
     struct _plist *prev;
     struct _plist *next;
 }plist;
 
-void plist_add_front(plist **head_ptr, double x, double y, double t){
+void plist_add_front(plist **head_ptr, double x, double vx, double y, double vy, double z, double vz, double t){
     plist *tmp = (plist*)malloc(sizeof(plist));
     tmp->x = x;
     tmp->y = y;
+    tmp->z = z;
+    tmp->vx = vx;
+    tmp->vy = vy;
+    tmp->vz = vz;
     tmp->t = t;
     tmp->next = *head_ptr;
     tmp->prev = NULL;
@@ -48,15 +64,37 @@ void plist_erase(plist **head){
 }
 
 void plist_evolve_ode(plist **head_ptr, plist **tail_ptr){
-    double x, y, t, k1, k2, k3, k4;
-    x = (*head_ptr)->x;
-    y = (*head_ptr)->y;
-    t = (*head_ptr)->t;
-    k1 = vanderpol(x,y,t)*dt;
-    k2 = vanderpol(x,y+k1/2,t+dt/2)*dt;
-    k3 = vanderpol(x,y+k2/2,t+dt/2)*dt;
-    k4 = vanderpol(x,y+k3,t+dt)*dt;
-    plist_add_front(head_ptr, x+y*dt, y+k1/6+k2/3+k3/3+k4/6, t+dt);
+    double x, vx, y, vy, z, vz, k1, k2, k3, k4;
+/*
+    k1 = vanderpol((*head_ptr)->x,(*head_ptr)->vx,(*head_ptr)->t)*dt;
+    k2 = vanderpol((*head_ptr)->x,(*head_ptr)->vx+k1/2,(*head_ptr)->t+dt/2)*dt;
+    k3 = vanderpol((*head_ptr)->x,(*head_ptr)->vx+k2/2,(*head_ptr)->t+dt/2)*dt;
+    k4 = vanderpol((*head_ptr)->x,(*head_ptr)->vx+k3,(*head_ptr)->t+dt)*dt;
+    vx += k1/6+k2/3+k3/3+k4/6;
+    x  += vx*dt;
+*/
+    vx = lorenz_x((*head_ptr)->x,(*head_ptr)->y,(*head_ptr)->z);
+    k1 = lorenz_x((*head_ptr)->x,(*head_ptr)->y,(*head_ptr)->z)*dt;
+    k2 = lorenz_x((*head_ptr)->x+k1/2,(*head_ptr)->y,(*head_ptr)->z)*dt;
+    k3 = lorenz_x((*head_ptr)->x+k2/2,(*head_ptr)->y,(*head_ptr)->z)*dt;
+    k4 = lorenz_x((*head_ptr)->x+k3,(*head_ptr)->y,(*head_ptr)->z)*dt;
+    x = (*head_ptr)->x + k1/6+k2/3+k3/3+k4/6;
+
+    vy = lorenz_y((*head_ptr)->x,(*head_ptr)->y,(*head_ptr)->z);
+    k1 = lorenz_y((*head_ptr)->x,(*head_ptr)->y,(*head_ptr)->z)*dt;
+    k2 = lorenz_y((*head_ptr)->x,(*head_ptr)->y+k1/2,(*head_ptr)->z)*dt;
+    k3 = lorenz_y((*head_ptr)->x,(*head_ptr)->y+k2/2,(*head_ptr)->z)*dt;
+    k4 = lorenz_y((*head_ptr)->x,(*head_ptr)->y+k3,(*head_ptr)->z)*dt;
+    y = (*head_ptr)->y + k1/6+k2/3+k3/3+k4/6;
+
+    vz = lorenz_z((*head_ptr)->x,(*head_ptr)->y,(*head_ptr)->z);
+    k1 = lorenz_z((*head_ptr)->x,(*head_ptr)->y,(*head_ptr)->z)*dt;
+    k2 = lorenz_z((*head_ptr)->x,(*head_ptr)->y,(*head_ptr)->z)*dt;
+    k3 = lorenz_z((*head_ptr)->x,(*head_ptr)->y,(*head_ptr)->z)*dt;
+    k4 = lorenz_z((*head_ptr)->x,(*head_ptr)->y,(*head_ptr)->z+k3)*dt;
+    z = (*head_ptr)->z + k1/6+k2/3+k3/3+k4/6;
+
+    plist_add_front(head_ptr, x, vx, y, vy, z, vz, 0);
 }
 
 #endif
--- ODE Viewer/viewer.c
+++ ODE Viewer/viewer.c
@@ -2,7 +2,7 @@
 #include "ode.h"
 #include "color.h"
 
-bool ACTIVE = true;
+int active = 1;
 
 plist *list = NULL;
 
@@ -16,7 +16,7 @@ void drawLine(plist *trail, rgb_t color)
     glBegin(GL_LINE_STRIP);
     glColor3d(color.r,color.g,color.b);
     while(trail != NULL){
-        glVertex2d(trail->x,trail->y);
+        glVertex3d(trail->x,trail->y,trail->z);
         trail = trail->next;
     }
     glEnd();
@@ -26,8 +26,9 @@ void drawCircle(plist head, double r, rgb_t color)
 {
     glColor3d(color.r,color.g,color.b);
     glBegin(GL_POLYGON);
-    for (int i = 0; i < 360; i++)
-        glVertex2d(head.x+r*cos(i*3.14159265/180), head.y+r*sin(i*3.14159265/180));
+    int i;
+    for(i = 0; i < 360; i++)
+        glVertex3d(head.y+r*cos(i*3.14159265/180), head.y+r*sin(i*3.14159265/180), head.z);
     glEnd();
 }
 
@@ -48,17 +49,17 @@ void reshapeF(int w,int h)
     glViewport(0,0,w,h);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glOrtho(-w*a/h+b.x,w*a/h+b.x,-a+b.y,a+b.y,1,-1);
+    glOrtho(-w*a/h+b.x,w*a/h+b.x,-a+b.y,a+b.y,a+b.z,-a+b.z);
     glClear(GL_COLOR_BUFFER_BIT);
     glClearColor(1,1,1,0);
 }
 
 void init()
 {
-    plist_add_front(&list,0,0,0);
+    plist_add_front(&list,0,0,0,0,0,0,0);
 
-    ACTIVE = true;
-    b.x = b.y = 0;
+    active = 1;
+    b.x = b.y = b.z = 0;
     ds = 0.05;
     a = 1;
 
@@ -72,21 +73,21 @@ void keyboardF(unsigned char key, int x, int y)
     switch(key)
     {
         case '+':
-            glTranslatef(b.x,b.y,0);
-            glScalef(scalef,scalef,1);
-            glTranslatef(-b.x,-b.y,0);
+            glTranslatef(b.x,b.y,b.z);
+            glScalef(scalef,scalef,scalef);
+            glTranslatef(-b.x,-b.y,-b.z);
             ds/=scalef;
             a/=scalef;
             break;
         case '-':
-            glTranslatef(b.x,b.y,0);
-            glScalef(1/scalef,1/scalef,1);
-            glTranslatef(-b.x,-b.y,0);
+            glTranslatef(b.x,b.y,b.z);
+            glScalef(1/scalef,1/scalef,1/scalef);
+            glTranslatef(-b.x,-b.y,-b.z);
             ds*=scalef;
             a*=scalef;
             break;
         case 'p': case 'P': case ' ':
-            ACTIVE=!ACTIVE;
+            active=!active;
             break;
         case 'f': case 'F':
             glutFullScreenToggle();
@@ -123,6 +124,14 @@ void specialKeyboardF(int key, int x, int y)
             glTranslatef(-ds,0,0);
             b.x+=ds;
             break;
+        case GLUT_KEY_PAGE_UP:
+            glTranslatef(0,0,-ds);
+            b.z+=ds;
+            break;
+        case GLUT_KEY_PAGE_DOWN:
+            glTranslatef(0,0,ds);
+            b.z-=ds;
+            break;
     }
 }
 
@@ -134,6 +143,7 @@ plist getPosition(int x, int y)
     plist tmp;
     tmp.x = ((x*2.0/viewport[2]-1)*viewport[2]/viewport[3]) * a + b.x;
     tmp.y = (2.0*y/viewport[3]-1) * a + b.y;
+    tmp.z = b.z;
     tmp.t = 0;
     return tmp;
 }
@@ -144,13 +154,13 @@ void mouseF(int button, int state, int x, int y)
     {
         plist tmp = getPosition(x,y);
         plist_erase(&list);
-        plist_add_front(&list, tmp.x, tmp.y, tmp.t);
+        plist_add_front(&list, tmp.x, tmp.y, tmp.z, 0, 0, 0, tmp.t);
     }
 }
 
 void idleF(void)
 {
-    if(ACTIVE)
+    if(active)
         plist_evolve_ode(&list, NULL);
     glutPostRedisplay();
 }
