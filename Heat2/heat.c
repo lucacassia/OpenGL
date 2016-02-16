29c2c7e8fc1031518b11eaef39b76748f6e8dd78
megadoro <6.626068h@gmail.com>
2016-02-16 18:43:57 +0100

    update

---
+++ Heat2/heat.c
@@ -0,0 +1,120 @@
+#include "heat.h"
+#include "../lib/color.h"
+
+#include <GL/freeglut.h>
+
+int ACTIVE = 0;
+
+void savePPM()
+{
+    FILE *f = fopen("screenshot.ppm", "wb");
+    fprintf(f, "P6\n%d %d\n255\n", WIDTH, HEIGHT);
+    unsigned int i,j;
+    rgb_t color;
+    unsigned char c[3];
+    for(i = 0; i < HEIGHT; i++)
+        for(j = 0; j < WIDTH; j++){
+            color = d2rgb(pixels[HEIGHT-i-1][j]);
+            c[0] = (char)(255*color.r);
+            c[1] = (char)(255*color.g);
+            c[2] = (char)(255*color.b);
+            fwrite(c, sizeof(unsigned char), 3, f);
+        }
+    fclose(f);
+    printf("\nPPM file saved to screenshot.ppm\n");
+}
+
+void GLInit()
+{
+    glDisable(GL_DEPTH_TEST);
+    glClearColor(0.0f ,0.0f, 0.0f, 0.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glOrtho(0.0, WIDTH, 0.0, HEIGHT, 0.0, 1.0);
+}
+
+void displayF()
+{
+    float* data = (float*)malloc(3 * size() * sizeof(float));
+
+    rgb_t color;
+    unsigned int i,j;
+    for(i = 0; i < HEIGHT; i++)
+        for(j = 0; j < WIDTH; j++){
+            color = d2rgb(pixels[CENTER]);
+            data[3*(i*WIDTH+j)+0] = color.r;
+            data[3*(i*WIDTH+j)+1] = color.g;
+            data[3*(i*WIDTH+j)+2] = color.b;
+        }
+    glRasterPos2i(0,0);
+    glDrawPixels(WIDTH,HEIGHT,GL_RGB,GL_FLOAT,data);
+    glutSwapBuffers();
+
+    free(data);
+}
+
+void idleF()
+{
+    if(ACTIVE){
+        evolve();
+        glutPostRedisplay();
+    }
+}
+
+void keyboardF(unsigned char key, int mouseX, int mouseY)
+{
+    switch(key)
+    {
+        case 'p': case 'P':
+            savePPM();
+            break;
+        case 'q': case 'Q': case 27:
+            clear();
+            printf("\n[EXIT]\n");
+            exit(EXIT_SUCCESS);
+        case ' ':
+            ACTIVE = !ACTIVE;
+            if(ACTIVE) printf("\n[ACTIVE]\n");
+            else printf("\n[PAUSED]\n");
+            break;
+        case '+':
+            DIFFUSION *= 1.1;
+            break;
+        case '-':
+            DIFFUSION /= 1.1;
+            break;
+    }
+}
+
+void mouseF(int button, int state, int x, int y)
+{
+    int i,j,viewport[4];
+    glGetIntegerv(GL_VIEWPORT, viewport);
+    y = viewport[3] - y;
+
+    if( ( y < HEIGHT ) && ( x < WIDTH ) )
+        if( button == GLUT_LEFT_BUTTON && state == GLUT_DOWN )
+            for(i = 0; i < HEIGHT; i++)
+                for(j = 0; j < WIDTH; j++)
+                    pixels[CENTER] += exp( -((x-j)*(x-j) + (y-i)*(y-i)) / sqrt( WIDTH * HEIGHT ) ); /* maybe old, dunno */
+}
+
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+
+    if(argc == 2)
+        initFile(argv[1]);
+    else
+        initRand(WIDTH,HEIGHT);
+
+    glutInitWindowSize(WIDTH,HEIGHT); 
+    glutInitDisplayMode( GLUT_RGB | GLUT_DOUBLE );
+    glutCreateWindow("Heat"); 
+    GLInit();
+    glutDisplayFunc(displayF); 
+    glutIdleFunc(idleF);
+    glutMouseFunc(mouseF);
+    glutKeyboardFunc(keyboardF);
+    glutMainLoop();
+    return 0;
+}
---
+++ Heat2/heat.h
@@ -0,0 +1,122 @@
+#ifndef HEAT
+#define HEAT
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include "../lib/color.h"
+
+double DIFFUSION = 0.5f;
+
+unsigned int WIDTH = 600;
+unsigned int HEIGHT = 600;
+double** pixels = NULL;
+double** old = NULL;
+double** source = NULL;
+
+#define RIGHT  i][(j+1)%WIDTH
+#define LEFT   i][(WIDTH+j-1)%WIDTH
+#define UP     (HEIGHT+i-1)%HEIGHT][j
+#define DOWN   (i+1)%HEIGHT][j
+#define CENTER i][j
+
+unsigned int width()  { return WIDTH; }
+unsigned int height() { return HEIGHT; }
+unsigned int size()   { return WIDTH * HEIGHT; }
+
+void clear()
+{
+    int i;
+
+    if(pixels){
+        for(i = 0; i < HEIGHT; i++)
+            if(pixels[i])
+                free(pixels[i]);
+        free(pixels);
+        pixels = NULL;
+    }
+
+    if(old){
+        for(i = 0; i < HEIGHT; i++)
+            if(old[i])
+                free(old[i]);
+        free(old);
+        old = NULL;
+    }
+
+    if(source){
+        for(i = 0; i < HEIGHT; i++)
+            if(source[i])
+                free(source[i]);
+        free(source);
+        source = NULL;
+    }
+}
+
+void initRand(unsigned int w, unsigned int h)
+{
+    clear();
+
+    srand(time(NULL));
+
+    WIDTH = w;
+    HEIGHT = h;
+
+    pixels = (double**)malloc(HEIGHT * sizeof(double*));
+    old = (double**)malloc(HEIGHT * sizeof(double*));
+    source = (double**)malloc(HEIGHT * sizeof(double*));
+
+    int i,j;
+
+    for(i = 0; i < HEIGHT; i++){
+        pixels[i] = (double*)malloc(WIDTH * sizeof(double));
+        old[i] = (double*)malloc(WIDTH * sizeof(double));
+        source[i] = (double*)malloc(WIDTH * sizeof(double));
+        for(j = 0; j < WIDTH; j++){
+            source[i][j] = 0;
+            old[i][j] = pixels[i][j] = rand()*1.0/RAND_MAX;
+        }
+    }
+}
+
+void initFile(const char* path)
+{
+    FILE* file = fopen(path, "rb");
+    char buf[256];
+    unsigned int i,j,d;
+    fgets(buf, 256, file);
+    do{ fgets(buf, 256, file); }while ( strncmp(buf, "#", 1) == 0 );
+    sscanf(buf, "%u %u", &WIDTH, &HEIGHT);
+    fscanf(file, "%u", &d);
+    fseek(file, 1, SEEK_CUR);
+
+    initRand(WIDTH,HEIGHT);
+
+    rgb_t color;
+    unsigned char cbuf[3];
+    for(i = 0; i < HEIGHT; i++)
+        for(j = 0; j < WIDTH; j++){
+            fread(cbuf, 1, 3, file);
+            color.r = cbuf[0]/255.0;
+            color.g = cbuf[1]/255.0;
+            color.b = cbuf[2]/255.0;
+            old[HEIGHT-1-i][j] = pixels[HEIGHT-1-i][j] = rgb2d(color);
+        }
+    fclose(file);
+}
+
+void evolve()
+{
+    int i,j;
+
+    for(i = 0; i < HEIGHT; i++)
+        for(j = 0; j < WIDTH; j++)
+            pixels[CENTER] += DIFFUSION * ( old[RIGHT] + old[UP] + old[LEFT] + old[DOWN] - 4 * old[CENTER] ) / 4 + source[CENTER];
+
+    for(i = 0; i < HEIGHT; i++)
+        for(j = 0; j < WIDTH; j++)
+            old[CENTER] = pixels[CENTER];
+}
+
+#endif
